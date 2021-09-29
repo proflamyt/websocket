3845c87ce3486e8fda25057bfc225fdc9683d2cb
proflamyt <proflamyt@gmail.com>
2021-09-30 02:58:34 +0800

    print environ variable

--- websoc-ola.py
+++ websoc-ola.py
@@ -8,23 +8,23 @@ import websockets
 
 
 async def echo(websocket, path):
-    while True:
-        #can only emit string
-        await websocket.send('ola')
-        await asyncio.sleep(1)
+    async for message in websocket:
+        await websocket.send(message)
+
 
 async def main():
     # Set the stop condition when receiving SIGTERM.
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
+    print(os.environ["PORT"])
     async with websockets.serve(
         echo,
         host="",
         port=int(os.environ["PORT"]),
     ):
         await stop
+    
 
 
 if __name__ == "__main__":
